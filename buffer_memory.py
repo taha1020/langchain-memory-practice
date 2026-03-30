efe600427cfc8c12e852b6f66e094e649602a250
Muhammad Taha Ahmad <165555538+taha1020@users.noreply.github.com>
2026-03-30 19:12:58 +0500

    added buffer memory

---
+++ buffer_memory.py
@@ -0,0 +1,11 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+
+llm = ChatOpenAI(temperature=0)
+
+memory = ConversationBufferMemory()
+conversation = ConversationChain(llm=llm, memory=memory)
+
+print(conversation.run("Hi, my name is Wahaj"))
+print(conversation.run("What is my name?"))
